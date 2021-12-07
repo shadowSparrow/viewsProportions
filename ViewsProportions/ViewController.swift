062bead8f80e2aa9e69d919d51297a8ba5463155
shadowSparrow <romanenko.aleksandre@gmail.com>
2021-12-08 00:46:57 +0300

    Initial Commit

---
+++ ViewsProportions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewsProportions
+//
+//  Created by mac on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
